402467fcca79b09b1a0155bd5014e9eccbc3332d
Gregor Seegers <gregor.seegers@gmail.com>
2021-03-03 11:37:30 +0100

    complete challenge 1

---
+++ challenge.js
@@ -0,0 +1,9 @@
+var challenge1 = [["Vienna", 1, 2, 3, 4],
+            [0, "is", 2, 3, 4],
+            [0, 1, "a", 3, 4],
+            [0, 1, 2, "nice", 4],
+            [0, 1, 2, 3, "city."]];
+
+
+document.write(challenge1[0][0] + " " + challenge1[1][1] + " " + challenge1[2][2] + " " + challenge1[3][3] + " " + challenge1[4][4]);
+
