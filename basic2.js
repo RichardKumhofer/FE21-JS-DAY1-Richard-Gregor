ec7dff90f4e543215962181e4567e06a845c74f1
Richard Kumhofer <richard.kumhofer@gmail.com>
2021-03-03 10:12:20 +0100

    basic done

---
+++ basic2.js
@@ -0,0 +1,7 @@
+var age = 25;
+
+console.log("Hi my name is Martin" + " and I am " + age + " years old");
+
+var players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"];
+
+console.log("The most valuable player of the match is " + players[2]);
